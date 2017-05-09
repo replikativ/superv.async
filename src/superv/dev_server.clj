cf2a2f1b652bf700e5dac6cdba8903a31170134e
Brian Marco <mail@brianmarco.com>
2017-05-09 11:08:08 -0400

    naive solution to resources index.hmtl problem

---
+++ src/superv/dev_server.clj
@@ -0,0 +1,23 @@
+(ns superv.dev-server
+  (:require
+    [ring.util.response :as resp])
+  (:gen-class))
+
+(defn handle-index [handler]
+  (fn [request]
+    (if (= [:get "/"] ((juxt :request-method :uri) request))
+      (resp/response
+        "<!DOCTYPE html>
+        <html>
+        <body>
+        <script src=\"js/client.js\"></script>
+        </body>
+        </html>")
+      (handler request)
+    )))
+
+(def main-handler
+  (-> {}
+      handle-index))
+
+
