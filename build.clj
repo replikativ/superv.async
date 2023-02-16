bf4c76c1a3b0cda3fb335146b162612d38aef990
Timo Kramer <fw-git@timokramer.de>
2023-02-16 14:08:21 +0100

    fixup! chore: deps update, script updates

--- build.clj
+++ build.clj
@@ -2,9 +2,11 @@
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]
             [borkdude.gh-release-artifact :as gh]
-            [org.corfield.build :as bb]))
+            [org.corfield.build :as bb])
+  (:import [clojure.lang ExceptionInfo]))
 
 (def lib 'io.replikativ/superv.async)
+(def current-commit (b/git-process {:git-args "rev-parse HEAD"}))
 (def version (format "0.3.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
@@ -46,13 +48,33 @@
       (assoc :lib lib :version version)
       (bb/deploy)))
 
+(defn fib [a b]
+  (lazy-seq (cons a (fib b (+ a b)))))
+
+(defn retry-with-fib-backoff [retries exec-fn test-fn]
+  (loop [idle-times (take retries (fib 1 2))]
+    (let [result (exec-fn)]
+      (if (test-fn result)
+        (when-let [sleep-ms (first idle-times)]
+          (println "Returned: " result)
+          (println "Retrying with remaining back-off times (in s): " idle-times)
+          (Thread/sleep (* 1000 sleep-ms))
+          (recur (rest idle-times)))
+        result))))
+
+(defn try-release []
+  (try (gh/overwrite-asset {:org "replikativ"
+                            :repo (name lib)
+                            :tag version
+                            :commit (current-commit)
+                            :file jar-file
+                            :content-type "application/java-archive"
+                            :draft false})
+       (catch ExceptionInfo e
+         (assoc (ex-data e) :failure? true))))
+
 (defn release
   [_]
-  (-> (gh/overwrite-asset {:org "replikativ"
-                           :repo (name lib)
-                           :tag version
-                           :commit (gh/current-commit)
-                           :file jar-file
-                           :content-type "application/java-archive"})
+  (-> (retry-with-fib-backoff 10 try-release :failure?)
       :url
       println))
